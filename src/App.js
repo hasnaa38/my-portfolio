9fa5e0a783e95d7abf7344d76d164ebbfcc87bc6
Hasnaa Al-Habahbeh <hasnaahab98@gmail.com>
2022-01-11 03:59:50 +0200

    edited the tab

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
@@ -7,6 +7,10 @@ import Skills from "./components/Skills";
 import Testimonials from "./components/Testimonials";
 
 export default function App() {
+  useEffect(() => {
+    document.title = "Hasnaa's Portfolio"
+  }, []);
+  
   return (
     < div className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
